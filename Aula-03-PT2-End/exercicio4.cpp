4b40f8a3fcf5e3ae7ff4128bd79f147c8988881f
Chrislima <christopher@inatel.br>
2022-01-11 12:04:45 -0300

    Arrumado comentario do exercicio4.cpp da aula 03

--- Aula-03-PT2-End/exercicio4.cpp
+++ Aula-03-PT2-End/exercicio4.cpp
@@ -8,10 +8,10 @@ int main(){
 	
 	v[3] = 30;
 	
-	//v[3] e *(v+3) sao equivalente. Ambos acessam o contedo da terceira posicao do vetor
+	//v[3] e *(v+3) sao equivalente. Ambos acessam o conteudo da terceira posicao do vetor
 	cout << "Valor de v[3]: "<< v[3] << " e valor de *(v+3): " << *(v+3) << endl; 
 	
-	//v[3] e *(v+3) sao equivalente. Ambos acessam o contedo da terceira posicao do vetor
+	//&v[3] e v+3 sao equivalente. Ambos acessam o endereco da terceira posicao do vetor
 	cout << "Valor de &v[3]: "<< &v[3] << " e valor de v+3: " << v+3 << endl; 
 
 	//&v[3] eh o endereco da terceira posicao do vetor. 
